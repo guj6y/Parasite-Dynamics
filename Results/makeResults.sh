228870e49015c2039eff3094dd2d7e1d4b135ba4
guj6y <nguj6y@gmail.com>
2017-10-27 09:21:15 -0700

    fixed a weird typo that was breaking initial typeset

--- Results/makeResults.sh
+++ Results/makeResults.sh
@@ -2,6 +2,7 @@
 
 redraw=
 convert=
+verbose=
 graphicsExtension=png
 while [ "$1" != "" ]; do
     case $1 in
@@ -25,6 +26,9 @@ while [ "$1" != "" ]; do
      -c | --forceReConversion)
          convert=true
          ;;
+     -v | --verbose)
+         verbose=true
+         ;;
     esac
     shift
 done
@@ -32,29 +36,50 @@ done
 
 if [ "$redraw" = true ]; then
     cd ../floats/results
-    echo "Making Figures ... "
-    latexmk -interaction=batchmode -pdf -silent > log
-    echo "Cleaning up ..."
-    latexmk -silent -c
+    if ["$verbose" = true]; then 
+        echo "Making Figures ... "
+        latexmk -interaction=batchmode -pdf >> log
+        echo "Cleaning up ..."
+        latexmk -c
+    else
+        echo "Making Figures ... "
+        latexmk -interaction=batchmode -pdf -silent >> log
+        echo "Cleaning up ..."
+        latexmk -silent -c
+    fi
 fi
 
 if [ "$convert" = true ]; then
     echo "Converting to .png ..."
-    for pdffile in *.pdf; do
+    if ["$verbose" = true]; then
+        for pdffile in *.pdf; do
+            convert -v -density 500 "${pdffile}" "${pdffile%.*}.$graphicsExtension"
+        done
+    else
+        for pdffile in *.pdf; do
         convert -density 500 "${pdffile}" "${pdffile%.*}.$graphicsExtension"
-    done
+        done
+    fi 
     cd ../../Results
     echo "Done with figures."
 fi
 echo "Typesetting Document as pdf ... "
-
-latexmk -interaction=batchmode -pdf -silent > log
+if ["$verbose" = true]; then
+    latexmk -interaction=batchmode -pdf -silent >> log
+else
+    latexmk -interaction=batchmode -pdf >> log
+fi
 
 echo "Converting document to .docx ..."
 
-for texfile in *.tex; do
-    pandoc -f latex -t docx -o "${texfile%.*}".docx --bibliography=Bib_green.bib -M link-citations=true "${texfile}"
-done
-
+if ["$verbose" = true]; then
+    for texfile in *.tex; do
+        pandoc -v -f latex -t docx -o "${texfile%.*}".docx --bibliography=Bib_green.bib -M link-citations=true "${texfile}"
+    done
+else
+    for texfile in *.tex; do
+        pandoc -f latex -t docx -o "${texfile%.*}".docx --bibliography=Bib_green.bib -M link-citations=true "${texfile}"
+    done
+fi
 echo "Cleaning up ..."
-latexmk -silent -c
+#latexmk -silent -c
