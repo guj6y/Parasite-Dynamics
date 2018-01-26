4dfce2bc9442415c61b72974f2274a45a62af38a
guj6y <nkappler@math.arizona.edu>
2018-01-26 11:18:16 -0600

    makeResults shell script updated and various logs and docx

--- report/sections/results/makeResults.sh
+++ report/sections/results/makeResults.sh
@@ -1,68 +1,69 @@
 #!/bin/bash
 
-redraw=
-convert=
-verbose=
-graphicsExtension=png
-while [ "$1" != "" ]; do
-    case $1 in
-     -r | --redraw)
-         redraw=true
-         convert=true
-         ;;
-     -jpg)
-         convert=true
-         graphicsExtension=jpg
-         ;;
-     -bmp)
-         convert=true
-         graphicsExtension=bmp
-         ;;
-     -g | --graphics-type)
-         convert=true
-         shift
-         graphicsExtension=$1
-         ;;
-     -c | --forceReConversion)
-         convert=true
-         ;;
-     -v | --verbose)
-         verbose=true
-         ;;
-    esac
-    shift
-done
-#Option for remaking figures
+#redraw=
+#convert=
+#verbose=
+#graphicsExtension=png
+#while [ "$1" != "" ]; do
+#    case $1 in
+#     -r | --redraw)
+#         redraw=true
+#         convert=true
+#         ;;
+#     -jpg)
+#         convert=true
+#         graphicsExtension=jpg
+#         ;;
+#     -bmp)
+#         convert=true
+#         graphicsExtension=bmp
+#         ;;
+#     -g | --graphics-type)
+#         convert=true
+#         shift
+#         graphicsExtension=$1
+#         ;;
+#     -c | --forceReConversion)
+#         convert=true
+#         ;;
+#     -v | --verbose)
+#         verbose=true
+#         ;;
+#    esac
+#    shift
+#done
+##Option for remaking figures
+#
+#if [ "$redraw" = true ]; then
+#    cd ../floats/results
+#    if [ "$verbose" = true ]; then 
+#        echo "Making Figures ... "
+#        latexmk -interaction=batchmode -pdf >> log
+#        echo "Cleaning up ..."
+#        latexmk -c
+#    else
+#        echo "Making Figures ... "
+#        latexmk -interaction=batchmode -pdf -silent >> log
+#        echo "Cleaning up ..."
+#        latexmk -silent -c
+#    fi
+#fi
+#
+#if [ "$convert" = true ]; then
+#    echo "Converting to .png ..."
+#    if [ "$verbose" = true  ]; then
+#        for pdffile in *.pdf; do
+#            convert -v -density 500 "${pdffile}" "${pdffile%.*}.$graphicsExtension"
+#        done
+#    else
+#        for pdffile in *.pdf; do
+#        convert -density 500 "${pdffile}" "${pdffile%.*}.$graphicsExtension"
+#        done
+#    fi 
+#    cd ../../Results
+#    echo "Done with figures."
+#fi
 
-if [ "$redraw" = true ]; then
-    cd ../floats/results
-    if [ "$verbose" = true ]; then 
-        echo "Making Figures ... "
-        latexmk -interaction=batchmode -pdf >> log
-        echo "Cleaning up ..."
-        latexmk -c
-    else
-        echo "Making Figures ... "
-        latexmk -interaction=batchmode -pdf -silent >> log
-        echo "Cleaning up ..."
-        latexmk -silent -c
-    fi
-fi
-
-if [ "$convert" = true ]; then
-    echo "Converting to .png ..."
-    if [ "$verbose" = true  ]; then
-        for pdffile in *.pdf; do
-            convert -v -density 500 "${pdffile}" "${pdffile%.*}.$graphicsExtension"
-        done
-    else
-        for pdffile in *.pdf; do
-        convert -density 500 "${pdffile}" "${pdffile%.*}.$graphicsExtension"
-        done
-    fi 
-    cd ../../Results
-    echo "Done with figures."
-fi
 echo "Typesetting Document as pdf ... "
 if [ "$verbose" = true ]; then
     latexmk -interaction=batchmode -pdf -silent >> log
