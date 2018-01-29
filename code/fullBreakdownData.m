6a58a7cc0daa4b48feb6ee528ca78f1cdc98ae48
guj6y <nguj6y@gmail.com>
2018-01-29 08:39:39 -0700

    added fixed fullBreakdown file to code folder

--- code/fullBreakdownData.m
+++ code/fullBreakdownData.m
@@ -16,8 +16,10 @@ selector = [1 1;
             1 2;
             2 1;
             2 2];
-header = 'yPerAll,sPerAll,yPerPara,sPerPara,yPerFree,sPerFree,yBioBasal,sBioBasal,yBioFree,sBioFree,yBioPara,sBioPara';
-fileFormat = repmat('%.9e,',12);
+header = 'x,yPerAll,sPerAll,yPerPara,sPerPara,yPerFree,sPerFree,yBioBasal,sBioBasal,yBioFree,sBioFree,yBioPara,sBioPara';
+fParAll = [0,0.025,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5];
+
+fileFormat = repmat('%.9e,',1,13);
 fileFormat = fileFormat(1:end-1);
 
 for plotNo = 1:4;    
@@ -25,13 +27,13 @@ for plotNo = 1:4;
     modelNo = selector(plotNo,1);
     freeSize = selector(plotNo,2);
 
-    perAll = squeeze(persistences.all(:,:,modelNo,modelNo,freeSize,:));
-    perPara = squeeze(persistences.para(:,:,modelNo,modelNo,freeSize,:));
-    perFree = squeeze(persistences.free(:,:,modelNo,modelNo,freeSize,:));
+    perAll = squeeze(persistences.all(:,:,freeSize,:,modelNo,modelNo));
+    perPara = squeeze(persistences.para(:,:,freeSize,:,modelNo,modelNo));
+    perFree = squeeze(persistences.free(:,:,freeSize,:,modelNo,modelNo));
 
-    bioPara = squeeze(biomasses.para(:,:,modelNo,modelNo,freeSize,:));
-    bioFree = squeeze(biomasses.free(:,:,modelNo,modelNo,freeSize,:));
-    bioBasal = squeeze(biomasses.basal(:,:,modelNo,modelNo,freeSize,:));
+    bioPara = squeeze(biomasses.para(:,:,freeSize,:,modelNo,modelNo));
+    bioFree = squeeze(biomasses.free(:,:,freeSize,:,modelNo,modelNo));
+    bioBasal = squeeze(biomasses.basal(:,:,freeSize,:,modelNo,modelNo));
 
 
     meanPerAll = squeeze(mean(perAll));
@@ -50,21 +52,21 @@ for plotNo = 1:4;
     stdBioPara = squeeze(std(bioPara));
     stdBioFree = squeeze(std(bioFree));
     
-    matrixSaveBigPara = [meanPerAll(:,1),stdPerAll(:,1),meanPerPara(:,1),stdPerPara(:,1),meanPerFree(:,1),stdPerFree(:,1),...
+    matrixSaveBigPara = [fParAll',meanPerAll(:,1),stdPerAll(:,1),meanPerPara(:,1),stdPerPara(:,1),meanPerFree(:,1),stdPerFree(:,1),...
                            meanBioBasal(:,1),stdBioBasal(:,1),meanBioFree(:,1),stdBioFree(:,1),meanBioPara(:,1),stdBioPara(:,1)];
 
-    matrixSaveSmallPara = [meanPerAll(:,2),stdPerAll(:,2),meanPerPara(:,2),stdPerPara(:,2),meanPerFree(:,2),stdPerFree(:,2),...
+    matrixSaveSmallPara = [fParAll',meanPerAll(:,2),stdPerAll(:,2),meanPerPara(:,2),stdPerPara(:,2),meanPerFree(:,2),stdPerFree(:,2),...
                            meanBioBasal(:,2),stdBioBasal(:,2),meanBioFree(:,2),stdBioFree(:,2),meanBioPara(:,2),stdBioPara(:,2)];
 
     
-    fileName = sprintf('%s-%s-bigPara',modelNames{modelNo},freeNames{freeNo});
-    fid = fopen(fileName);
+    fileName = sprintf('%s-%s-bigPara',modelNames{modelNo},freeNames{freeSize});
+    fid = fopen(fileName,'w');
     fprintf(fid,'%s\n',header);
     fprintf(fid,sprintf('%s\n',fileFormat),matrixSaveBigPara');
     fclose(fid);
 
-    fileName = sprintf('%s-%s-smallPara',modelNames{modelNo},freeNames{freeNo});
-    fid = fopen(fileName);
+    fileName = sprintf('%s-%s-smallPara',modelNames{modelNo},freeNames{freeSize});
+    fid = fopen(fileName,'w');
     fprintf(fid,'%s\n',header);
     fprintf(fid,sprintf('%s\n',fileFormat),matrixSaveSmallPara');
     fclose(fid);
